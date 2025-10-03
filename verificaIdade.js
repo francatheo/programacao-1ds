885337c17c04496d746271968a8e79f1a3592183
francatheo <teixeira.theo@escola.pr.gov.br>
2025-10-03 15:15:40 -0300

    verificaIdade.js

---
+++ verificaIdade.js
@@ -0,0 +1,11 @@
+// Programa Verificador de Idade
+
+// 1. Criar uma variável para armazenar a idade
+let idade = 17; // Você pode alterar esse valor para testar diferentes idades
+
+// 2. Estrutura condicional para verificar se é maior ou menor de idade
+if (idade >= 18) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é menor de idade.");
+}
