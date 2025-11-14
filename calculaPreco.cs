30b62e72cad4786503fb4d2f48d7b84a0d5f7df1
francatheo <teixeira.theo@escola.pr.gov.br>
2025-11-14 15:09:29 -0300

    calculaPreco.cs

---
+++ calculaPreco.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+    public double Desconto { get; set; }
+
+    public double CalcularPrecoFinal()
+    {
+        return Preco - (Preco * Desconto / 100);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Produto produto = new Produto
+        {
+            Nome = "Fone de ouvido",
+            Preco = 200,
+            Desconto = 25
+        };
+
+        // Exibe o preço inicial
+        Console.WriteLine($"Preço inicial de {produto.Nome}: R$ {produto.Preco:F2}");
+
+        // Exibe o preço final
+        Console.WriteLine($"Preço final de {produto.Nome}: R$ {produto.CalcularPrecoFinal():F2}");
+    }
+}
